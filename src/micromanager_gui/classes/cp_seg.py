94df8e4b414b1f68294f027f73781e887073b73c
Anna Yang <annayangzq26@gmail.com>
2024-06-13 16:51:35 -0400

    add unet model as comparison

--- src/micromanager_gui/classes/cp_seg.py
+++ src/micromanager_gui/classes/cp_seg.py
@@ -1,60 +1,68 @@
-from cellpose import io, models, plot
+import tensorflow as tf
+import tensorflow.keras.backend as K
 from pathlib import Path
 import numpy as np
 import random
 import os
+from skimage import feature, filters, morphology, segmentation
+from scipy import ndimage as ndi
 
 class SegmentNeurons():
     """Segment neurons in the image."""
     def __init__(self) -> None:
-        self._model: models.CellposeModel = None
+        self._model: tf.keras.models = None
 
-    def _load_model(self) -> None:
+    def _load_model(self, img_size: int) -> None:
         """Load the model"""
-        print("_________Loading cellpose model__________")
-        dir_path = Path(__file__).parent
-        model_path = Path.joinpath(dir_path, "CP_calcium")
-        self._model = models.CellposeModel(gpu=False,
-                                                pretrained_model=model_path)
+        # only initiate the trained model once
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        path = os.path.join(dir_path, f'unet_calcium_{img_size}.hdf5')
+        self._model = tf.keras.models.load_model(path, custom_objects={"K": K})
         
-    def _segment(self, img: np.ndarray, path: str, channels: list = [0,0]) -> np.ndarray:
-        """Segmetnt the img."""
-        masks, flows, _ = self._model.eval(img,
-                                        diameter=None,
-                                        flow_threshold=0.1,
-                                        cellprob_threshold=0,
-                                        channels=channels)
+    def _segment(self, img_stack, minsize, background_label):
+        '''
+        Predict the cell bodies using the trained NN model
+        and further segment after removing small holes and objects
 
-        if not os.path.isdir(path):
-            os.mkdir(path)
+        Parameters:
+        -------------
+        img_stack: ndarray. shape=(# of frames, # of rows, # of colomns) array of the images
+        minsize: float or int. the threshold to determine if the predicted cell body is too small
+        background_label: int. the value set to be the background
 
-        self._save_overlay(img, channels, masks, path)
-        rgb_mask = plot.mask_rgb(masks)
-        save_path = os.path.join(path, "masks.tif")
-        io.save_masks(img, rgb_mask, flows, save_path, tif=True)
+        Return:
+        -------------
+        labels: ndarray. shape= shape of img_stack. a labeled matrix of segmentations of the same type as markers
+        label_layer: ndarray. the label/segmentation layer image
+        roi_dict: dict. a dictionary of label-position pairs
+        '''
+        img_norm = np.max(img_stack, axis=0) / np.max(img_stack)
+        img_predict = self._model.predict(img_norm[np.newaxis, :, :])[0, :, :]
 
-        return masks
+        if np.max(img_predict) > 0.3:
+            # use Otsu's method to find the cooridnates of the cell bodies
+            th = filters.threshold_otsu(img_predict)
+            img_predict_th = img_predict > th
+            img_predict_remove_holes_th = morphology.remove_small_holes(img_predict_th, area_threshold=minsize * 0.3)
+            img_predict_filtered_th = morphology.remove_small_objects(img_predict_remove_holes_th, min_size=minsize)
+            distance = ndi.distance_transform_edt(img_predict_filtered_th)
+            local_max = feature.peak_local_max(distance,
+                                               min_distance=10,
+                                               footprint=np.ones((15, 15)),
+                                               labels=img_predict_filtered_th)
+
+            # create masks over the predicted cell bodies and add a segmentation layer
+            local_max_mask = np.zeros_like(img_predict_filtered_th, dtype=bool)
+            local_max_mask[tuple(local_max.T)] = True
+            markers = morphology.label(local_max_mask)
+            labels = segmentation.watershed(-distance, markers, mask=img_predict_filtered_th)
+            roi_dict, labels = self._getROIpos(labels, background_label)
+
+        return labels, roi_dict
 
     def _check_model(self):
         """Check if the model is loaded."""
         return self._model is not None
-
-    def _save_overlay(self, img: np.ndarray, channels: list, 
-                      masks: np.ndarray, save_path: str) -> None:
-        """Save the overlay image of masks over original image."""
-        img0 = img.copy()
-
-        if img0.shape[0] < 4:
-            img0 = np.transpose(img0, (1, 2, 0))
-        if img0.shape[-1] < 3 or img0.ndim < 3:
-            img0 = plot.image_to_rgb(img0, channels=channels)
-        else:
-            if img0.max() <= 50.0:
-                img0 = np.uint8(np.clip(img0, 0, 1) * 255)
-
-        # generate mask over original image
-        overlay = plot.mask_overlay(img0, masks)
-        io.imsave(os.path.join(save_path, "overlay.jpg"), overlay)
 
     def _getROIpos(self, masks: list | np.ndarray, bg_label: int = 0) -> tuple[dict, np.ndarray]:
         """Get pos of ROIs"""
@@ -74,32 +82,54 @@ class SegmentNeurons():
         # delete any background labels
         del roi_dict[bg_label]
 
-        # area_dict, roi_to_delete = self.get_ROI_area(roi_dict, 100)
+        area_dict, roi_to_delete = self.get_ROI_area(roi_dict, 100)
 
-        # # delete roi in label layer and dict
-        # for r in roi_to_delete:
-        #     coords_to_delete = np.array(roi_dict[r]).T.tolist()
-        #     masks[tuple(coords_to_delete)] = 0
-        #     roi_dict[r] = []
+        # delete roi in label layer and dict
+        for r in roi_to_delete:
+            coords_to_delete = np.array(roi_dict[r]).T.tolist()
+            masks[tuple(coords_to_delete)] = 0
+            roi_dict[r] = []
 
-        # # move roi in roi_dict after removing some labels
-        # for r in range(1, (len(roi_dict) - len(roi_to_delete) + 1)):
-        #     i = 1
-        #     while not roi_dict[r]:
-        #         roi_dict[r] = roi_dict[r + i]
-        #         roi_dict[r + i] = []
-        #         i += 1
+        # move roi in roi_dict after removing some labels
+        for r in range(1, (len(roi_dict) - len(roi_to_delete) + 1)):
+            i = 1
+            while not roi_dict[r]:
+                roi_dict[r] = roi_dict[r + i]
+                roi_dict[r + i] = []
+                i += 1
 
-        # # delete extra roi keys
-        # for r in range((len(roi_dict) - len(roi_to_delete) + 1), (len(roi_dict) + 1)):
-        #     del roi_dict[r]
+        # delete extra roi keys
+        for r in range((len(roi_dict) - len(roi_to_delete) + 1), (len(roi_dict) + 1)):
+            del roi_dict[r]
 
-        # # update label layer with new roi
-        # for r in roi_dict:
-        #     roi_coords = np.array(roi_dict[r]).T.tolist()
-        #     masks[tuple(roi_coords)] = r
+        # update label layer with new roi
+        for r in roi_dict:
+            roi_coords = np.array(roi_dict[r]).T.tolist()
+            masks[tuple(roi_coords)] = r
 
         return roi_dict, masks
+
+    def get_ROI_area(self, roi_dict: dict, threshold: float):
+        '''
+        to get the areas of each ROI in the ROI_dict
+
+        Parameters:
+        ------------
+        roi_dict: dict. the dictionary of the segmented ROIs
+        threshold: float or int. The value below which the segmentation would be considered small
+
+        Returns:
+        -----------
+        area: dict. a dictionary of the length of the coordinates (?) in the roi_dict
+        small_roi: list. a list of small rois if their coordinates are smaller than the set threshold
+        '''
+        area = {}
+        small_roi = []
+        for r in roi_dict:
+            area[r] = len(roi_dict[r])
+            if area[r] < threshold:
+                small_roi.append(r)
+        return area, small_roi
     
     def _ROI_intensity(self, roi_dict: dict, img_stack: np.ndarray) -> dict:
         """Calculate raw signal averaged across all pixels in the ROI."""
@@ -141,13 +171,8 @@ class SegmentNeurons():
     def _run(self, img: np.ndarray, file_path: str) -> tuple[dict, dict]:
         """Run the entire segmentation process."""
         print("__________Segmenting___________")
-        masks = self._segment(img[100, :, :], file_path)
-        roi_dict, masks = self._getROIpos(masks, 0)
+        labels, roi_dict = self._segment(img[10], file_path)
         raw_signal = self._ROI_intensity(roi_dict, img)
         dff, median, bg = self._calculate_DFF(raw_signal)
 
         return roi_dict, raw_signal, dff
-
-    def _run_evk(self):
-        """Run segmentation for evoked activity."""
-        #TODO: to be implemented
