fffcc5c16d0caa7efdc8f719e8515776b6e9df26
Anna Yang <annayangzq26@gmail.com>
2024-08-29 18:17:31 -0400

    fix: check if data or meta is None

--- src/micromanager_gui/_batch_analysis.py
+++ src/micromanager_gui/_batch_analysis.py
@@ -295,8 +295,10 @@ def _analyze(
 
         # get the data
         stack, meta = data.isel(p=p, metadata=True)
-        if stack is None:
-            return
+        if stack is None or meta is None:
+            print("No labels found for %s!", p)
+            continue
+
         # get position name from metadata
         pos_name = meta[0].get("Event", {}).get("pos_name", f"pos_{str(p).zfill(4)}")
 
